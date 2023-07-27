38a37d73a5003c8d69365fad8c5867cd7a64a892
Salami Oluwadamilola <105321247+TiminiOfLagos@users.noreply.github.com>
2023-07-27 23:02:10 +0100

    Add files via upload

---
+++ src/Details.js
@@ -0,0 +1,201 @@
+// Enter all your detials in this file
+
+// Profile Image
+import profile from "./assets/Timmy.JPG";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import Netflix from "./assets/projects/Netflix.png"
+import BMI from "./assets/projects/BMI.png"
+import AgeCal from "./assets/projects/Agecal.jpg"
+import ColorPal from "./assets/projects/color-palette.jpg"
+
+
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Salami Oluwadamilola",
+
+  tagline: "I am a Software Developer and a UI/UX Product Designer",
+
+  img: profile,
+
+  about : `Hello, I'm Damilola, a versatile and passionate professional with a unique 
+  blend of expertise in product design and an entry level knowledge in software development. With a keen eye
+  for detail and a deep appreciation for innovation, I strive to create seamless user 
+  experiences that bridge the gap between form and function.`,
+
+  firstStack : `As a software developer, I relish the opportunity to bring ideas to 
+  life through code. My journey in the world of programming began Febuary 2022 and for a year and some couple of months, 
+  I have honed my skills in a variety of programming languages and frameworks, allowing me to build robust, 
+  efficient, and scalable applications. My hands-on experience in [mention specific areas, 
+  e.g., web development, mobile apps, etc.] has instilled in me a problem-solving 
+  mindset and an insatiable curiosity to stay at the forefront of technological advancements.`,
+
+  secondStack : `Being a product designer, I believe in the magic of design thinking. I'm captivated 
+  by the power of creating meaningful connections with users by crafting intuitive and aesthetically 
+  pleasing interfaces. My approach to design involves an empathetic understanding of user needs and 
+  behavior, which leads to the development of captivating visual experiences. My proficiency in 
+  industry-standard design tools enables me to effectively communicate ideas and transform them 
+  into tangible, delightful products.`,
+
+  rolesCombine : `What truly sets me apart is the seamless fusion of my skills as a software developer 
+  and product designer. This interdisciplinary mindset allows me to embark on projects with a holistic 
+  view, translating ideas into products that are not only technically exceptional but also visually 
+  compelling. Whether I'm working on developing a new application from scratch or enhancing an existing 
+  design, I continuously seek ways to enrich user interactions and create a lasting impact.`,
+
+  collab : `I thrive in collaborative environments where diverse talents converge to achieve a shared vision. 
+  Working with cross-functional teams has shown me the incredible results that emerge when we pool our creativity, 
+  expertise, and experiences. I am always eager to engage in exciting projects and embrace new challenges that push 
+  the boundaries of what technology and design can achieve together.`
+
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/oluwadamilola-salami-4563b623a/",
+  github: "https://www.github.com/TiminiOfLagos",
+  twitter: "https://twitter.com/Slamzofficial",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Quality Assurance Analyst Intern",
+    Company: `CWAY`,
+    Location: "Ajao Estate, Lagos",
+    Type: "Internship",
+    Duration: "May 2021 - September 2021",
+  },
+  {
+    Position: "Office Assitant",
+    Company: `Oyasal Global Ventures`,
+    Location: "Lagos,Nigeria",
+    Type: "Full Time",
+    Duration: "August 2016 - September 2019",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Software Engineering",
+    Company: "Sail Innovation Lab X CChub",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "Febuary 2023 - Present",
+  },
+  {
+    Position: "Software Engineering",
+    Company: "TechCampus Africa",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "April 2022 - July 2022",
+  },
+  {
+    Position: "Product Design",
+    Company: "Zuri Training X Ingressive For Good",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "March 2022 - August 2022",
+  },
+  {
+    Position: "National Diploma in Science Laboratory Technology",
+    Company: `Lagos State Polytechnic`,
+    Location: "Ikorodu,Lagos",
+    Type: "Full Time",
+    Duration: "October 2019 - December 2021",
+    Honor: "Distinction - Honors Roll"
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Netflix Landing Page Clone",
+    image: Netflix,
+    description: `This is a netflix landing page clone i worked on `,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://merry-moonbeam-39efe0.netlify.app/",
+    githubLink: "https://github.com/TiminiOfLagos/Netflix-Clone",
+  },
+  {
+    title: "Body Mass Index Calculator",
+    image: BMI,
+    description: `Body mass index calculator`,
+    techstack: "React JS, CSS",
+    previewLink: "https://stirring-pothos-f3aaec.netlify.app/",
+    githubLink: "https://github.com/TiminiOfLagos/Body-mass-calculator/tree/main",
+  },
+  {
+    title: "Age Calculator",
+    image: AgeCal,
+    description: `This is an age calculator`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://strong-maamoul-b495eb.netlify.app/",
+    githubLink: "https://github.com/TiminiOfLagos/AG-Cal/tree/main",
+  },
+  {
+    title: "Color Flipper",
+    image: ColorPal,
+    description: `This is a color change project on click of a button`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://inquisitive-otter-ec88c3.netlify.app/",
+    githubLink: "https://github.com/TiminiOfLagos/Color-Flipper",
+  },
+//   {
+//     title: "Project title 5",
+//     image: projectImage5,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+//   {
+//     title: "Project title 6",
+//     image: projectImage6,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "salamitimileyin001@gmail.com",
+  phone: "(+234) 8164938612",
+};
+
